4036f0e3a5198db7e3935028751ec34309f6a7ca
Passakorn340 <75597156+Passakorn340@users.noreply.github.com>
2021-03-12 12:33:08 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,6 +36,7 @@ export default {
         .signOut()
         .then(() => {
           console.log("signOut Complete");
+          alert("SignOut Complete");
         })
         .catch((error) => {
           console.log(error);
